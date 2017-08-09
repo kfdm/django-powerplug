0096ca15ca8efc7e93be57cd2e852f58fdbe9cd1
Paul Traylor <paul.traylor@linecorp.com>
2017-08-10 08:25:40 +0900

    Add logging to loader

--- powerplug/tasks.py
+++ powerplug/tasks.py
@@ -9,6 +9,7 @@ from pkg_resources import working_set
 logger = logging.getLogger(__name__)
 
 for entry in working_set.iter_entry_points('powerplug.task'):
+    logger.info('Loading %s', entry)
     try:
         entry.load()
     except ImportError:
